bc06fc6ff8ba99d0f6151bfd1944f65017f62cbd
Mirzaazmath <azmath9603399270@gmail.com>
2025-12-01 16:10:44 +0530

    added native code

--- android/app/src/main/kotlin/com/example/futter_detect_dev_mode/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/futter_detect_dev_mode/MainActivity.kt
@@ -2,4 +2,26 @@ package com.example.futter_detect_dev_mode
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity(){
+
+    private  val METHOD = "developerOption";
+    override  fun configureFlutterEngine(flutterEngine:FlutterEngine){
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecuter.binaryMessenger,METHOD).setMethodCallHandler{
+            call,result ->
+            when(call.method){
+                "isDevMode"->{
+                    val  devEnabled = Settings.Secure.getInt(contentResoler,Settings.Global.DEVELOPMENT_SETTING_ENABLED,0)==1
+                    result.success(devEnabled)
+                }
+
+                "isUsbDebugging"->{
+                    val usbEnabled = Settings.Secure.getInt(contentResoler,Settings.Secure.ADB_ENABLED,0)==1
+                    result.success(usbEnabled)
+                }
+            }
+        }
+    }
+
+}
