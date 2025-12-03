1b06b636d8bfaa7f5ae1a6dbcb7641d91a340383
Mirzaazmath <azmath9603399270@gmail.com>
2025-12-03 16:43:20 +0530

    fixed all and tested all

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,32 +10,65 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>with WidgetsBindingObserver {
   bool devOptions = false;
   bool usbDebugging = false;
+  bool isDialogOpen = false;
   @override
   void initState() {
     initCheck();
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
   }
 
   void initCheck() async {
     devOptions = await DeveloperOptionService.isDeveloperOptionEnabled();
     usbDebugging = await DeveloperOptionService.isUsbDebuggingEnabled();
 
-    if (devOptions) {
+    if (devOptions || usbDebugging) {
+      isDialogOpen = true;
       showDialog(
         context: context,
         barrierDismissible: false,
         builder: (BuildContext context) {
-          return CustomDialogBox(
-           usbDebugging: usbDebugging,
+          return PopScope(
+            canPop: false,
+            child: CustomDialogBox(
+              usbDebugging: usbDebugging,
+            ),
           );
         },
       );
     }
   }
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state)async {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed) {
+      print('App is in resumed state ');
+      // Recheck developer settings
+      bool dev = await DeveloperOptionService.isDeveloperOptionEnabled();
+      bool usb = await DeveloperOptionService.isUsbDebuggingEnabled();
 
+      // If both OFF → close dialog
+      if (!dev && !usb && isDialogOpen) {
+        Navigator.of(context, rootNavigator: true).pop(); // Close dialog
+        isDialogOpen = false;
+      }
+    } else if (state == AppLifecycleState.paused) {
+      print('App is in paused state (background)');
+      // Perform actions when the app goes to the background, e.g., save data, pause tasks.
+    } else if (state == AppLifecycleState.inactive) {
+      print('App is in inactive state (e.g., system dialog opened)');
+    } else if (state == AppLifecycleState.detached) {
+      print('App is in detached state (e.g., app closed)');
+    }
+  }
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(child: Text("Hello World")));
